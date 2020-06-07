355955bcb94cadef395ebf5b6b78ac1e2decc46a
jiang_11 <2455082067@qq.com>
2020-06-07 15:45:16 +0800

    创建分支

--- HelloGit/src/main/java/com/jiang_11/git/HelloGit.java
+++ HelloGit/src/main/java/com/jiang_11/git/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
     public static void mian(String[] args){
         System.out.println("Hello Git!");
         System.out.println("更新1");
+        System.out.println("创建分支");
     }
 }
