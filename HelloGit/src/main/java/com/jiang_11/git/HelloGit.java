0cb66a09774a7951c7c032e5a8b9c97dd1416301
jiang_11 <2455082067@qq.com>
2020-06-07 20:15:49 +0800

    测试ssh

--- HelloGit/src/main/java/com/jiang_11/git/HelloGit.java
+++ HelloGit/src/main/java/com/jiang_11/git/HelloGit.java
@@ -10,9 +10,11 @@ public class HelloGit {
         System.out.println("更新1");
         System.out.println("创建分支");
         System.out.println("主干添加");
+        System.out.println("分支添加");
         System.out.println("在线更新");
         System.out.println("克隆下来的项目添加");
         System.out.println("克隆下来的项目添加");
         System.out.println("克隆下来的项目添加");
+        System.out.println("测试ssh");
     }
 }
